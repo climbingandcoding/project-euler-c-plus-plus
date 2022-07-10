5606bc738f95216a8d264940c57c0dd61b276e22
climbingandcoding <108996474+climbingandcoding@users.noreply.github.com>
2022-07-09 21:11:06 -0500

    Update Problem10.cpp

--- Problem10.cpp
+++ Problem10.cpp
@@ -6,42 +6,42 @@ using namespace std;
 bool isPrime(int number)
 {
   
-	if (number < 2)
+  if (number < 2)
   {
     return false;
   }
 
-	if (number % 2 == 0)
+  if (number % 2 == 0)
   {
     return number == 2;
   }
 		
-	if (number % 3 == 0)
+  if (number % 3 == 0)
   {
     return number == 3;
   }
 
-	if (number % 5 == 0)
+  if (number % 5 == 0)
   {
     return number == 5;
   }
 
-	if (number % 7 == 0)
+  if (number % 7 == 0)
   {
     return number == 7;
   }
 
-	for (int i = 11; i * i <= number; i++) 
+  for (int i = 11; i * i <= number; i++) 
   {
     
-		if (number % i == 0)
+    if (number % i == 0)
     {
       return false;
     }
     
-	}
+  }
 
-	return true;
+  return true;
   
 }
 
@@ -49,19 +49,19 @@ bool isPrime(int number)
 int main()
 {
   
-	long long sum = 0;
+  long long sum = 0;
 
   // Loops through all digits and adds the current value to sum if it is prime
-	for (int i = 2; i < 2000000; i++) 
+  for (int i = 2; i < 2000000; i++) 
   {
     
-		if (isPrime(i))
+    if (isPrime(i))
     {
       sum += i;
     }
     
-	}
+  }
 
-	cout << "The answer is: " << sum << endl;
+  cout << "The answer is: " << sum << endl;
   
 }
