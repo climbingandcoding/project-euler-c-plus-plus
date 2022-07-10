358f5cbe5b03280b7a4c48c241592a37a20ba7be
climbingandcoding <108996474+climbingandcoding@users.noreply.github.com>
2022-07-10 08:27:44 -0500

    Update Problem13.cpp

--- Problem13.cpp
+++ Problem13.cpp
@@ -7,14 +7,14 @@ int main()
 {
   
   long answer = 0;
-  char current[ 50 + 1 ];
+  char current[50 + 1];
 
   // While there is something there
-  while(scanf( "%s", current ) == 1) 
+  while(scanf("%s", current) == 1) 
   {
     
     // We only need three more, becauese 10 * 50 = 500
-    current[ 10 + 3 ] = '\0';
+    current[10 + 3] = '\0';
 
     long temp;
     sscanf(current, "%ld", &temp);
