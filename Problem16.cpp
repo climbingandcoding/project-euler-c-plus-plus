0209d079f2e8c5ae985231c33d2dbb082bc910e5
climbingandcoding <108996474+climbingandcoding@users.noreply.github.com>
2022-07-10 22:36:54 -0500

    Update Problem16.cpp

--- Problem16.cpp
+++ Problem16.cpp
@@ -13,24 +13,24 @@ int main()
   cout << "Enter a number: ";
   cin >> power;
   
-	double number = pow(2, power);
-	int answer = 0;
+  double number = pow(2, power);
+  int answer = 0;
 
   // Goes through entire 'number' string
-	for (char ch : to_string(number)) 
+  for (char ch : to_string(number)) 
   {
 
     // Skip to next character if current is 0 or non-numeric
-		if (ch == '0' || !isdigit(ch))
+    if (ch == '0' || !isdigit(ch))
     {
       continue;
     }
 
     // 48 is '0', which keeps answer in 'string' format
-		answer = answer + static_cast<int>(ch) - 48;
+    answer = answer + static_cast<int>(ch) - 48;
     
-	}
+  }
   
-	cout << "The answer is: " << answer << '\n';
+    cout << "The answer is: " << answer << '\n';
   
 }
